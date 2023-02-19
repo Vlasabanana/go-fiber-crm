a9ad25ad019d15b297deb391395dccee0fa74a3f
Vlasabanana <vlasabanana@yandex.ru>
2023-02-19 19:50:42 +0300

    st

--- lead/lead.go
+++ lead/lead.go
@@ -2,9 +2,9 @@ package lead
 
 import (
 	"github.com/Vlasabanana/go-fiber-crm/database"
+	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"github.com/gofiber/fiber"
 )
 
 type Lead struct {
@@ -16,18 +16,30 @@ type Lead struct {
 }
 
 func GetLeads(c *fiber.Ctx) {
+	db := datablase.DBConn
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
+}
+
+func GetLead(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+	var lead lead
+	db.Find(&lead, id)
+	c.JSON(lead)
+}
+
+func NewLead(c *fiber.Ctx) {
+	db := database.DBConn
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
 
 }
 
-func GetLead() {
+func DeleteLead(c *fiber.Ctx) {
 
 }
-
-func NewLead() {
-
-}
-
-func DeleteLead() {
-
-}
-
